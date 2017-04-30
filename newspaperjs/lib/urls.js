bb5fe0355ddee0bc324a0cfd141dc56fedb2307c
flickz <sheaflickz@gmail.com>
2017-04-30 16:25:50 +0100

    Updated file structure

---
+++ newspaperjs/lib/urls.js
@@ -0,0 +1,11 @@
+'use strict'
+const url = require('url');
+
+const ALLOWED_TYPES = ['html', 'htm', 'md', 'rst', 'aspx', 'jsp', 'rhtml', 'cgi',
+                 'xhtml', 'jhtml', 'asp'];
+
+const GOOD_PATHS = ['story', 'article', 'feature', 'featured', 'slides',
+              'slideshow', 'gallery', 'news', 'video', 'media',
+              'v', 'radio', 'press'];
+
+const BAD_DOMAINS = ['amazon', 'doubleclick', 'twitter'];
