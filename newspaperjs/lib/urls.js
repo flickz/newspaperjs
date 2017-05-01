4f53877b70dfbfdab41f528f63d4466c1f2a88a3
flickz <sheaflickz@gmail.com>
2017-05-01 14:53:05 +0100

    added removeArg to url module

--- newspaperjs/lib/urls.js
+++ newspaperjs/lib/urls.js
@@ -1,6 +1,6 @@
 'use strict'
-const url = require('url');
-
+const urlPaser = require('url');
+const _ = require('lodash');
 const ALLOWED_TYPES = ['html', 'htm', 'md', 'rst', 'aspx', 'jsp', 'rhtml', 'cgi',
                  'xhtml', 'jhtml', 'asp'];
 
@@ -9,3 +9,32 @@ const GOOD_PATHS = ['story', 'article', 'feature', 'featured', 'slides',
               'v', 'radio', 'press'];
 
 const BAD_DOMAINS = ['amazon', 'doubleclick', 'twitter'];
+/**
+ * Returns url without argument or with specified param argument
+ * @param {String} url - Url to remove 
+ * @param {Array} [keep_param] - Params to keep
+ * @returns {String} - Modified url
+ */
+exports.removeArgs = function(url, keep_params=[]){
+    let parsedUrl = urlPaser.parse(url, true);
+    let filtered_query = ((query)=>{
+        if(!Array.isArray(keep_params)){
+            throw new TypeError("Array expected");
+        }
+        if(keep_params.length < 1){
+            return null
+        }
+        return  _.pick(query, keep_params);
+    })(parsedUrl.query);
+    return urlPaser.format({
+        protocol: parsedUrl.protocol,
+        slashes: parsedUrl.slashes,
+        auth: parsedUrl.auth,
+        host: parsedUrl.host,
+        port: parsedUrl.port,
+        hash: parsedUrl.hash,
+        query: filtered_query,
+        pathname: parsedUrl.pathname
+    });
+   
+}
