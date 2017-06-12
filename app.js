45ad91c8bc9764ef167cb8f5f8e9a6fe1328598c
flickz <sheaflickz@gmail.com>
2017-06-12 15:40:05 +0100

    removed app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const Build = require('./newspaperjs').Build;
-
-Build.getCategoriesUrl('http://www.indiatimes.com/', ['sports']).then(result=>{
-    console.log(result);
-}).catch(reason=>{
-    console.log(reason);
-})
